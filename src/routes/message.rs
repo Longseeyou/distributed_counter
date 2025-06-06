2b8f760bac6f728f37ed77951303e0a2d3dae540
LongDinh <91897962+Longseeyou@users.noreply.github.com>
2025-06-06 10:40:11 +0700

    change routes

--- src/routes/message.rs
+++ src/routes/message.rs
@@ -8,8 +8,9 @@ use crate::handlers::*;
 pub fn routes(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("/message")
-            .route("", post().to(produce_message))
-            .route("", get().to(get_viewers))
+            .route("/join", post().to(join_channel))
+            .route("/leave", post().to(leave_channel))
+            .route("/viewers", get().to(get_viewers))
             
     );
 }
